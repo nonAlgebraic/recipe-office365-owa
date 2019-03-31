bae215fe5dd861591b0d469cce9ce6991fc08cf5
nonAlgebraic <sine@sinewave.me>
2019-03-31 16:04:44 -0400

    Added support for new version of Outlook web app

--- webview.js
+++ webview.js
@@ -1,7 +1,33 @@
-module.exports = (Franz) => {
-    const getMessages = function getMessages() {
-        const unreadMail = jQuery("span[title*='Inbox'] + div > span").first().text();
-        Franz.setBadge(unreadMail);
-    };
-    Franz.loop(getMessages);
+'use strict';
+
+module.exports = Franz => {
+  const getMessages = function getMessages() {
+    let unreadMail = 0;
+    console.log('foo');
+    if (location.pathname.match(/\/owa/)) {
+      // classic app
+      unreadMail = parseInt(
+        jQuery("span[title*='Inbox'] + div > span")
+          .first()
+          .text(),
+        10
+      );
+    } else {
+      // new app
+      const folders = document.querySelector('div[title="Folders"]');
+      if (!folders) {
+        return;
+      }
+
+      unreadMail = [...folders.parentNode.children].reduce((count, child) => {
+        const unread = child.querySelector('.screenReaderOnly');
+        return unread && unread.textContent === 'unread'
+          ? count + parseInt(unread.previousSibling.textContent, 10)
+          : count;
+      }, 0);
+    }
+
+    Franz.setBadge(unreadMail);
+  };
+  Franz.loop(getMessages);
 };
